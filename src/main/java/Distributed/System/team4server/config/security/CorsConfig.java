2579dcc01ecb9592aad03be3e21e9309b262625d
kangCoder <khsofficial1213@gmail.com>
2023-06-01 11:10:17 +0900

    Fix: cors Authorization header 허용 설정

--- src/main/java/Distributed/System/team4server/config/security/CorsConfig.java
+++ src/main/java/Distributed/System/team4server/config/security/CorsConfig.java
@@ -16,6 +16,7 @@ public class CorsConfig {
         config.setAllowCredentials(true);
         config.addAllowedOriginPattern("*");
         config.addAllowedHeader("*");
+        config.addExposedHeader("Authorization");
         config.addAllowedMethod("*");
 
         source.registerCorsConfiguration("/**", config);
