2ec020d0a696150592c755acd9e536ff9bd48906
kangCoder <khsofficial1213@gmail.com>
2023-06-03 20:48:38 +0900

    Fix: CorsConfig 수정

--- src/main/java/Distributed/System/team4server/config/security/CorsConfig.java
+++ src/main/java/Distributed/System/team4server/config/security/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.addAllowedOrigin("http://localhost:3000");
-        config.addAllowedOriginPattern("*");
+        //config.addAllowedOriginPattern("*");
         config.addAllowedHeader("*");
         config.addExposedHeader("Authorization");
         config.addAllowedMethod("*");
